3ffde109ddb5bf15dd44d57565857f751718da2f
95-ag <96833386+95-ag@users.noreply.github.com>
2026-05-21 09:09:59 +0000

    feat: add content-copy and check material icons

---
+++ src/components/icons/material/check.tsx
@@ -0,0 +1,10 @@
+import { IconBase, type IconProps } from "@/components/icons/icon-base";
+
+export function CheckIcon(props: IconProps) {
+  return (
+    <IconBase
+      {...props}
+      path="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"
+    />
+  );
+}
